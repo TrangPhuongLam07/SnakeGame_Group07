19c8f94d53f73b13bd1c4d01c96910287794a513
TrangKwon <lptrangit@gmail.com>
2023-07-03 13:14:19 +0700

    update swamp food

--- src/vn/edu/nlu/model/Menu.java
+++ src/vn/edu/nlu/model/Menu.java
@@ -1,9 +1,13 @@
 package vn.edu.nlu.model;
 
 public interface Menu {
-public void newGame();
-public void levelGame();
-public void scoreGame();
-public void helpGame();
-public void exitGame();
+	public void newGame();
+
+	public void levelGame();
+
+	public void scoreGame();
+
+	public void helpGame();
+
+	public void exitGame();
 }
